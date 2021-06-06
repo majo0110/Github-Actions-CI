3d4b4b31c44b85ba0fc9807c172890ceef21430f
Jonathan <majo@oregonstate.edu>
2021-06-06 13:39:05 -0700

    Added test and examlpe file

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import example
+
+class TestCase(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(example.add(10, 15), 25)
+
+if __name__ == '__main__':
+    unittest.main()

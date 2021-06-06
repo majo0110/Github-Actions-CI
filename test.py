7b9cefbde4c813f7375aa9b4269efcd49581cf5b
Jonathan <majo@oregonstate.edu>
2021-06-06 13:44:48 -0700

    Fixed lint errors

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
 import example
 
+
 class TestCase(unittest.TestCase):
+
 
     def test_add(self):
         self.assertEqual(example.add(10, 15), 25)
+
 
 if __name__ == '__main__':
     unittest.main()
